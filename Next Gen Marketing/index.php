27d14de6408e4a8191d3bb95f83e6752229ade8a
Next Gen <99698542+xXNextGenXx@users.noreply.github.com>
2022-02-26 22:13:58 -0300

    Add files via upload

---
+++ Next Gen Marketing/index.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<title>Next Gen - Agência de Marketing Digital | Fortaleza -CE </title>
+	<meta charset="utf-8">
+	<meta name="keywords" content="agencia de marketing digital,agência de marketing,automação de marketing,criação de sites,páginas de venda,consultoria em marketing">
+	<meta name="author" content="Next Gen">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0">
+</head>
+<body>
+	<header>
+		<div class="container">
+			<div class="menu-topo">
+				<p style="color: #FFFFFF; text-shadow: 3px 5px 2px #474747; font-size: 40px;">Next Gen</p>
+				<nav class="desktop">
+					<ul>
+						<li><a href="">Sobre</a></li>
+						<li><a href="">Serviços</a></li>
+						<li><a href="">Contato</a></li>
+						<li class="btn-nav"><a href="">Quero um orçamento</a></li>
+					</ul>
+				</nav><!--desktop-->
+				<nav class="mobile">
+					<h3><i class="fa fa-bars"></i></h3>
+					<ul>
+						<li><a href="">Sobre</a></li>
+						<li><a href="">Serviços</a></li>
+						<li><a href="">Contato</a></li>
+						<li class="btn-nav"><a href="">Quero um orçamento</a></li>
+					</ul>
+				</nav><!--mobile-->
+				<div class="clear"></div>
+			</div><!--menu-topo-->
+			<div class="chamada-header">
+				<h2>Você vendendo mais<br />com automação web.</h2>
+				<p>Nós criamos soluções digitais para sua empresa vender mais, da melhor forma.</p>
+				<!--form active campaign aqui-->
+				<form>
+					<input type="email" required="" placeholder="Seu e-mail...">
+					<input type="submit" name="acao" value="Quero saber mais!">
+				</form>
+			</div>
+		</div><!--container-->
+	</header>
+
+	<section class="descricao-NextGen">
+		<div class="container">
+		<p>Com pouco tempo de experiência mais muita vontade de fazer a difernça em desenvolvimento web e automação de marketing, a <b>Next Gen</b> traz soluções que ajudam o seu Negócio a vender mais e alcançar públicos que você jamais imaginou alcançar.</p>
+		</div>
+	</section>
+
+	<section class="descricao-danki2">
+		<div class="container">
+			<div class="texto-descricao">
+				<h3>Nós somos uma agência digital. Trabalhamos seu posicionamento, conteúdo e estética, sempre focado em conversão.</h3>
+				<p>Converse com a nossa equipe ainda hoje e crie um plano de marketing para o seu negócio!</p>
+			</div>
+
+			<img alt="Trabalho NextGen" title="Trabalho Next" src="images/mockup1.jpg" />
+			<div class="clear"></div>
+		</div>
+	</section>
+
+	<section class="descricao-danki3">
+		<div class="container">
+			<div class="box-depoimento">
+				<p>“Temos a Danki como parceiro estratégico nos nossos planejamento em plataforma digital. Sempre nos trazendo soluções inovadoras e adequadas às nossas necessidades e dos nossos clientes. Super recomendo!“</p>
+				<br/>
+				<p><b>Glaúcio - Voodoo Branded Content</b></p>
+			</div>
+		</div>
+	</section>
+
+	<section class="servicos">
+		<div class="container">
+			<div class="title-servicos">
+				<h3>Como podemos ajudar sua empresa<br /><i class=" fa fa-angle-down"></i></h3>
+			</div>
+			<div class="flex-servicos">
+
+			<div class="space-servico">
+				<div class="box-servico">
+				<img src="images/img3.jpg" />	
+						<div class="box-servico-wraper">
+							<h2>E-mail Marketing</h2>
+							<p>Aprenda como utilizar e-mail marketing na sua empresa para alavancar suas vendas de uma forma automatizada.</p>
+							<a href="#">Orçamento</a>
+						</div><!--box-servico-wraper-->
+				</div><!--box-servico-->
+			</div><!--space-servico-->
+
+
+			<div class="space-servico">
+				<div class="box-servico">
+				<img src="images/img1.jpg" />	
+						<div class="box-servico-wraper">
+							<h2>Páginas de Venda</h2>
+							<p>Criamos páginas de venda 100% otimizadas e voltadas para conversões. Solicite um orçamento e tenha hoje mesmo a sua página para vender na internet.</p>
+							<a href="#">Orçamento</a>
+						</div><!--box-servico-wraper-->
+				</div><!--box-servico-->
+			</div><!--space-servico-->
+
+
+			<div class="space-servico">
+				<div class="box-servico">
+				<img src="images/img2.jpg" />	
+						<div class="box-servico-wraper">
+							<h2>Páginas de Captura e Inscrição</h2>
+							<p>Capture o e-mail dos visitantes em seu site e crie uma base de leads para sua empresa.</p>
+							<a href="#">Orçamento</a>
+						</div><!--box-servico-wraper-->
+				</div><!--box-servico-->
+			</div><!--space-servico-->	
+
+			<div class="space-servico">
+				<div class="box-servico">
+				<img src="images/img4.jpg" />	
+						<div class="box-servico-wraper">
+							<h2>SEO & Marketing de Conteúdo</h2>
+							<p>Aplicamos SEO no seu website para o mesmo ficar bem posicionado nos mecanismos de busca e ser achado facilmente.</p>
+							<a href="#">Orçamento</a>
+						</div><!--box-servico-wraper-->
+				</div><!--box-servico-->
+			</div><!--space-servico-->
+
+
+			<div class="space-servico">
+				<div class="box-servico">
+				<img src="images/img5.jpg" />	
+						<div class="box-servico-wraper">
+							<h2>ADWords & Facebook ADS</h2>
+							<p>Criamos campanhas altamente otimizadas para o seu negócio gerar mais vendas pelo menor custo.</p>
+							<a href="#">Orçamento</a>
+						</div><!--box-servico-wraper-->
+				</div><!--box-servico-->
+			</div><!--space-servico-->
+
+			<div class="space-servico">
+				<div class="box-servico">
+				<img src="images/img6.jpg" />	
+						<div class="box-servico-wraper">
+							<h2>Consultoria de Marketing Digital</h2>
+							<p>Precisa de ajuda e um sistema digital para alavancar o crescimento no seu negócio? Fale com a Next Gen, hoje mesmo, para
+							criarmos um sistema Web e Android para aumentar sua clientela e eficiência dos seus negócios..</p>
+							<a href="#">Orçamento</a>
+						</div><!--box-servico-wraper-->
+				</div><!--box-servico-->
+			</div><!--space-servico-->					
+
+			
+			</div><!--flex-servicos-->
+		</div>
+	</section>
+
+	<section class="portfolio">
+		<div class="container">
+			<h2>Algumas empresas que tivemos a oportunidade de atuar.</h2>
+			<div class="empresas">
+				<div class="empresa-single">
+					<img src="images/bianchini.png" />
+				</div><!--empresa-single-->
+				<div class="empresa-single">
+					<img src="images/bianchini.png" />
+				</div><!--empresa-single-->
+				<div class="empresa-single">
+					<img src="images/bianchini.png" />
+				</div><!--empresa-single-->
+				<div class="empresa-single">
+					<img src="images/bianchini.png" />
+				</div><!--empresa-single-->
+			</div><!--empresas-->
+		</div>
+	</section>
+
+	<section class="formulario">
+		<div class="container">
+			<h2>Um orçamento ou alguma dúvida?</h2>
+			<form>
+				<input type="text" name="nome" placeholder="Nome..." required>
+				<input type="text" name="email" placeholder="E-mail..." required>
+				<input type="text" name="telefone" placeholder="Telefone..." required>
+				<textarea name="mensagem" placeholder="Sua mensagem..."></textarea>
+				<input type="submit"  name="acao" value="Enviar!">
+			</form>
+		</div>
+	</section>
+
+	<footer>
+		<div class="container">
+			<div class="box-footer">
+				<h2>Endereço</h2>
+				<p>Rua 541, 22 - Conjunto Ceará, Fortaleza/CE 60531-450, Brasil</p>
+			</div>
+			<div class="box-footer">
+				<h2>Telefone</h2>
+				<p>(85)98852-3560</p>
+			</div>
+			<div class="box-footer">
+				<h2>Receba nossas news</h2>
+				<form>
+					<input placeholder="E-mail..." type="text" /><button type="submit"><i class="fa fa-paper-plane-o"></i></button>
+				</form>
+			</div>
+			<div class="clear"></div>
+		</div>
+	</footer>
+
+	
+	<!----------------------------------------->
+	<!--TODO: CRIAR SCROLL PARA FORM DE CONTATO!-->
+	<script src="js/jquery.js"></script>
+	<script>
+		$('nav.mobile h3').click(function(){
+			$('nav.mobile').find('ul').slideToggle();
+		})
+	</script>
+</body>
+</html>
